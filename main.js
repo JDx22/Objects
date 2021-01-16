e3e713629831f2b886492f324525bde66d7c863c
JDx <abirkoby@gmail.com>
2021-01-17 00:36:10 +0200

    ex 5.2

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ const reservations = {
     Ted: { claimed: true }
   }
   
-const name = prompt('Please enter the name for your reservation');
-if (reservations[name]==undefined)
+const name = prompt('Please enter the name for your reservation').toLowerCase();
+const editedName = name.charAt(0).toUpperCase() +name.slice(1);
+console.log(editedName);
+if (reservations[editedName]==undefined)
 {
   alert("You have no reservation");
-  reservations[name] = { claimed: true };
+  reservations[editedName] = { claimed: true };
   console.log(reservations);
 }
-else if (reservations[name].claimed)
+else if (reservations[editedName].claimed)
   alert("Hmm, someone already claimed this reservation");
 else 
-  alert("Welcome "+name);
+  alert("Welcome "+editedName);
